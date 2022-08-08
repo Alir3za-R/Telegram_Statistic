42bc53e3c8b4629a4aa176ce58e68826f785d8a5
Alir3za-R <rostami.alireza1277@gmail.com>
2022-08-08 22:09:50 +0430

    Add User Stats generator

--- src/chat_statistics/stats.py
+++ src/chat_statistics/stats.py
@@ -1,9 +1,10 @@
 import json
+from collections import Counter, defaultdict
 from pathlib import Path
 
 import arabic_reshaper
 from bidi.algorithm import get_display
-from hazm import Normalizer, word_tokenize
+from hazm import Normalizer, word_tokenize, sent_tokenize
 from loguru import logger
 from src.Data import DATA_DIR
 from wordcloud import WordCloud
@@ -40,8 +41,59 @@ class ChatStatistics:
         wordcloud = WordCloud(width=600, height=500, font_path=str(DATA_DIR/'BHoma.ttf'), background_color='White').generate(text_content)
         wordcloud.to_file(str(output/'WORD_CLOUD.png'))
 
+    def rebuild_msg(self, content):
+        """ Rebuild Messages that has mentions in them
+        """
+        text=""
+        for sub_msg in content:
+            if sub_msg is str:
+                text += sub_msg
+            elif sub_msg is dict:
+                text += sub_msg['text']
+        return text
+    def is_question(self, msg):
+        sentences = ""
+        content = ""
+        if not isinstance(msg['text'], str):
+            content = self.rebuild_msg(msg['text'])
+        else:
+            content = msg['text']
+        sentences += f" {' '.join(sent_tokenize(content))}"
+        for sentence in sentences:
+            if '?' in sentence or '؟' in sentence:
+                return True
+        return False
+
+    def generate_stats(self):
+        questions_id = defaultdict(bool)
+        self.Users = {}
+        logger.info('Evaluating Users Stats...')
+        # Extracting stats from messages
+        for msg in self.chat['messages']:
+            questions_id[msg['id']] = self.is_question(msg)
+            if not 'reply_to_message_id' in msg:
+                continue
+            if questions_id[msg['reply_to_message_id']] is False:
+                continue
+            if not msg['from_id'] in self.Users:
+                self.Users[msg['from_id']] = {
+                    'from': msg['from'],
+                    'reply_to_id': [],
+                    'id': []
+                }
+            self.Users[msg['from_id']]['reply_to_id'].append(msg['reply_to_message_id'])
+            self.Users[msg['from_id']]['id'].append(msg['id'])
+        return self.Users
+    def get_top_ten(self, top_n = 10):
+        logger.info(f'Getting Top {top_n} Users...')
+        helper_users = defaultdict(int)
+        for user in self.Users.values():
+            helper_users[user['from']] = len(user['reply_to_id'])
+        return dict(Counter(helper_users).most_common(top_n))
 
 if __name__ == '__main__':
-    tele_stats = ChatStatistics(DATA_DIR/'SM_Group.json')
-    tele_stats.generate_wordcloud(DATA_DIR)
+    tele_stats = ChatStatistics(DATA_DIR/'Pytopia.json')
+    #tele_stats.generate_wordcloud(DATA_DIR)
+    Users = tele_stats.generate_stats()
+    print(tele_stats.get_top_ten())
     print('Done!')
